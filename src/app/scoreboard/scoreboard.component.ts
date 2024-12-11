743dfcbbbeb1e018e247391a3ca135d29f965dbf
VasquezDav <davasquez7@miners.utep.edu>
2024-12-10 17:37:12 -0700

    added scoreboard functions

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ScoreboardService } from '../services/scoreboard.service';  // Import the service
 
 @Component({
   selector: 'app-scoreboard',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scoreboard.component.css']
 })
 export class ScoreboardComponent implements OnInit {
+  name: string = '';
+  score: number = 0;
+  scoreboard: { name: string; score: number }[] = [];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private scoreboardService: ScoreboardService 
+  ) {}
 
   ngOnInit(): void {
+    this.scoreboard = this.scoreboardService.getScores();
+    this.route.queryParams.subscribe((params) => {
+      const queryName = params['name'];
+      const queryScore = params['score'];
+      if (queryName && queryScore && !isNaN(+queryScore)) {
+        this.addScore(queryName, +queryScore);
+      }
+    });
   }
 
+  addScore(name: string, score: number): void {
+    if (name && score !== null) {
+      this.scoreboardService.addScore(name, score);
+      this.scoreboard = this.scoreboardService.getScores();  
+    }
+  }
 }
